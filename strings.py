28b296135b3f36b5ac725b9f1981cfc60d88abcc
marciorps <marciorps@yahoo.com,.br>
2023-11-22 10:42:48 -0300

    strings

---
+++ strings.py
@@ -0,0 +1,19 @@
+print('Codar todos os dias!')
+print("Codar todos os dias!!!")
+print('''Estamos codando todos os dias
+      E estou aprendendo muito!''')
+
+# Caracteres de escape
+print('Olá neu no é \nMárcio')
+print('Codar todos \' os dias')
+print('Arquivo localizado em \\c:drive\\arquivo1.txt')
+
+nome = 'carol'
+# função len conta caracteres
+print(len(nome))
+
+# Dsafio imprimir exatamento assim:
+
+print('Vamos codar!')
+print("Vamos 'codar'")
+print("Vamos \n'codar'")
